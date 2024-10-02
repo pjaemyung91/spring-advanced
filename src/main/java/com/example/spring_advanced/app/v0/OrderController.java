733b0d7db4528e9d7e554f6eb6e2fffe82ce698f
Jaemyung <p.jaemyung91@gmail.com>
2024-10-02 15:30:53 +0900

    init project

---
+++ src/main/java/com/example/spring_advanced/app/v0/OrderController.java
@@ -0,0 +1,18 @@
+package com.example.spring_advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderServiceV0 orderService;
+
+    @GetMapping("/v0/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+}
