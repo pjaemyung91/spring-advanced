f0fd534c29d446083790a8e6344d0b0f12db029e
Jaemyung <p.jaemyung91@gmail.com>
2024-10-04 17:15:10 +0900

    FEAT: log 추적기
    1. v0, v1 추가

--- src/main/java/com/example/spring_advanced/app/v0/OrderControllerV0.java
+++ src/main/java/com/example/spring_advanced/app/v0/OrderControllerV0.java
@@ -1,12 +1,12 @@
 package com.example.spring_advanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-public class OrderController {
+public class OrderControllerV0 {
 
     private final OrderServiceV0 orderService;
 
